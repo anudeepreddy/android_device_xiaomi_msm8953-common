c64dd2aa97aa6bcb653a1aa719ca03b2272944c3
Rashed Abdel-Tawab <rashed@linux.com>
2018-01-07 14:01:18 -0800

    msm8953-common: Add QTI privileged app permissions
    Change-Id: Ide1b79d83624e8aba2ee17979efacc356dfacb55

--- msm8953.mk
+++ msm8953.mk
@@ -376,7 +376,8 @@ PRODUCT_PACKAGES += \
 
 # Whitelisted app
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml \
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml
 
 # VR
 #PRODUCT_PACKAGES += \
