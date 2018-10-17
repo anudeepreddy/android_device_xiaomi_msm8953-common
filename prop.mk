d4fd71214dcf117e5a53bb5d062dbee6751e3be7
Dede Dindin Qudsy <xtrymind@gmail.com>
2018-10-17 07:22:13 +0700

    msm8953-common: Enable perf_hint_window
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

--- prop.mk
+++ prop.mk
@@ -88,7 +88,8 @@ ro.qualcomm.cabl=0 \
 ro.sf.lcd_density=480 \
 sdm.debug.disable_skip_validate=1 \
 vendor.gralloc.enable_fb_ubwc=1 \
-vendor.display.disable_skip_validate=1
+vendor.display.disable_skip_validate=1 \
+vendor.display.perf_hint_window=50
 
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
