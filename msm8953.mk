34270813b9c85409a0e6c8f4950e4be376eb8c13
Rama Bondan Prakoso <ramanarubp@gmail.com>
2018-10-25 00:50:17 +0700

    msm8953-common: makefile: Cleanup audio and camera

--- msm8953.mk
+++ msm8953.mk
@@ -88,7 +88,6 @@ PRODUCT_PACKAGES += \
     libqcomvisualizer \
     libqcomvoiceprocessing \
     libqcompostprocbundle \
-    libvolumelistener \
     tinymix
 
 # Audio configuration
@@ -113,11 +112,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    camera.msm8953 \
-    libmm-qcamera \
-    Snap \
     vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor
+    vendor.qti.hardware.camera.device@1.0_vendor \
+    Snap
 
 # Configstore
 PRODUCT_PACKAGES += \
