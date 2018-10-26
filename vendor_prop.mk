aa2fdb31a1648fce1b1432aecda558db14dea678
AmulyaX <amulya.b520@gmail.com>
2018-10-27 02:20:53 +0700

    msm8953-common: Enable HAL3 by default

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -69,6 +69,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     camera.lowpower.record.enable=1 \
     media.camera.ts.monotonic=1 \
     persist.camera.gyro.disable=0 \
+    persist.camera.HAL3.enabled=1 \
     persist.camera.isp.clock.optmz=0 \
     persist.camera.stats.test=5 \
     persist.vendor.qti.telephony.vt_cam_interface=1 \
